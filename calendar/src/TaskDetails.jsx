19afa1e77006b4a79172a18aaaf139305180db5d
sofiaparreira <sofiapparreira@gmail.com>
2024-12-03 17:19:15 -0300

    instalação e implementação do react-router-dom, criação do modal de detalhes da tarefa
    abrindo modal ao clicar na tarefa, estilo do modal quase finalizado

---
+++ calendar/src/TaskDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskDetails = () => {
+  return (
+    <div className='mx-16 my-8'>
+        
+      
+    </div>
+  )
+}
+
+export default TaskDetails
---
+++ calendar/src/components/TaskDetailModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const TaskDetailModal = () => {
+  return (
+      <div
+        className="fixed inset-0 z-10 bg-gray-500/75 flex items-center justify-center p-4"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="relative w-full max-w-5xl transform rounded-lg bg-white shadow-xl px-16 py-8 ">
+          <div className="flex justify-between">
+            <h3 className="text-xl font-semibold text-gray-800" id="modal-title">
+              Título da tarefa
+            </h3>
+            <span className="px-8 bg-green-50 text-green-500 items-center rounded-md flex">Teste</span>
+          </div>
+
+
+          <span className='text-gray-400 text-sm my-2'>28/01</span>
+          <p className='mt-12 border border-gray-200 rounded p-2 text-gray-800 min-h-32'>Descrição</p>
+          
+        </div>
+      </div>
+  )
+}
+
+export default TaskDetailModal
